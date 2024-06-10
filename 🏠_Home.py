d0db5204fc603491300c4ec8a6cd1f224d8de5d2
Bright Kwarteng Senior Adu <adubrightkwarrteng11@gmail.com>
2024-06-10 16:06:57 +0000

    Done creating the functionality of the  predict_page

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -1,4 +1,9 @@
-import streamlit as st
+#get libraries
+import streamlit as st 
+import requests
+import json
+from streamlit_lottie import st_lottie
+from streamlit_extras.switch_page_button import switch_page
 
 st.set_page_config(
     page_title ='Home Page',
@@ -6,6 +11,60 @@ st.set_page_config(
     layout="wide"
 )
 
-st.title("Churn Predictor")
-name = st.text_input("What is your name?")
-st.write(f"Hello {name}")
+
+#define function to get animation
+def lottie_url(url:str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+
+lottie_img = lottie_url("https://lottie.host/80d6a368-c787-4f59-8eca-9b649cf41b1b/VdfzfJeXsp.json")
+
+
+#intro talking about title 
+with st.container():
+    st.title("Unveiling secrets contributing to customer churn")
+    st.write("##")
+    st.write("""Every company wants to increase its profit or revenue margin and customer retention is one key area industry players 
+             focus their resources - and we at Vodafone are no different!!""")
+with st.container():
+    st.write("---")
+    col1, col2 = st.columns(2)
+    with col1:
+        st.header("About us")
+        st.write("##")
+        st.write("""
+                 Our group of experts in the team operate with the following objectives:
+
+                 - Explore our clients data thoroughly and decide on the most efficient classification models.
+                 - Find the lifetime value of each customer and know what factors affect the rate at which customers stop using their network.
+                 - Predict if a customer will churn or not.""")
+    with col2:
+        st_lottie(
+    lottie_img,
+    speed=1,
+    reverse= False,
+    loop=True,
+    quality="high",
+    key="coding",
+    height=500,
+    width=600
+
+)
+
+with st.container():
+        st.write("---")
+        st.header("Explore")
+        st.write("##")
+        st.write("With our powerful machine learning algorithms, you could also try to predict whether a customer will churn or not with you own dataset!")
+        data_button = st.button("Upload your data",key="data")
+        if data_button:
+            switch_page("Data")  
+        
+        
+        
+
+
+
